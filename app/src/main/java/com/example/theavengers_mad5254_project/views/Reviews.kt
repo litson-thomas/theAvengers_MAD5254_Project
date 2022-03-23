f6e085f16f09ce833497e73db9f3d1bbd69e80c4
lcmaze <lcmazeweb@gmail.com>
2022-03-23 17:03:01 -0400

    created the common header and created the reviews page with the review card item

---
+++ app/src/main/java/com/example/theavengers_mad5254_project/views/Reviews.kt
@@ -0,0 +1,12 @@
+package com.example.theavengers_mad5254_project.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.theavengers_mad5254_project.R
+
+class Reviews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reviews)
+    }
+}
