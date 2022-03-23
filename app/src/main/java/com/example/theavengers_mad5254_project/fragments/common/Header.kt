8d8efffc04b960972d14672b59f4340e3770d79f
lcmaze <lcmazeweb@gmail.com>
2022-03-23 15:12:11 -0400

    created the common header

---
+++ app/src/main/java/com/example/theavengers_mad5254_project/fragments/common/Header.kt
@@ -0,0 +1,12 @@
+package com.example.theavengers_mad5254_project.fragments.common
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.theavengers_mad5254_project.R
+
+class Header : Fragment(R.layout.fragment_header) {
+
+}
