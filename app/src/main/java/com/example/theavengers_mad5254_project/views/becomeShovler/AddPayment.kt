8dc052d7bc2dbb54192df4590382d6ad3d293404
unknown <vergel@gmail.com>
2022-03-24 19:30:10 -0400

    Add Become a Shovler and Shovler Pages

---
+++ app/src/main/java/com/example/theavengers_mad5254_project/views/becomeShovler/AddPayment.kt
@@ -0,0 +1,11 @@
+package com.example.theavengers_mad5254_project.views.becomeShovler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_payment)
+    }
+}
