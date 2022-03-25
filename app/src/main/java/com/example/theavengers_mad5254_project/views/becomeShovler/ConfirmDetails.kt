1541561a54a79d8e37dff8bb627e74493d02ea09
unknown <vergel@gmail.com>
2022-03-24 20:06:41 -0400

    Become Shovler2

--- app/src/main/java/com/example/theavengers_mad5254_project/views/becomeShovler/ConfirmDetails.kt
+++ app/src/main/java/com/example/theavengers_mad5254_project/views/becomeShovler/ConfirmDetails.kt
@@ -2,6 +2,7 @@ package com.example.theavengers_mad5254_project.views.becomeShovler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.theavengers_mad5254_project.R
 
 class ConfirmDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/theavengers_mad5254_project/views/shovlerDashboard/ShovlerDashboard.kt
+++ app/src/main/java/com/example/theavengers_mad5254_project/views/shovlerDashboard/ShovlerDashboard.kt
@@ -2,6 +2,7 @@ package com.example.theavengers_mad5254_project.views.shovlerDashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.theavengers_mad5254_project.R
 
 class ShovlerDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
